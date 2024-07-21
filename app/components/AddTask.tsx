f48d0bdffa4b92fd15ed4b111130525ffabcf743
yeen <yeenxdd@gmail.com>
2024-07-21 15:51:26 +0800

    Add update and delete operations

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -3,14 +3,24 @@
 import React, { FormEventHandler, useState } from 'react'
 import { FaPlus } from 'react-icons/fa'
 import Modal from './Modal'
+import { addTodo } from '@/api';
+import { useRouter } from 'next/navigation';
+import { v4 as uuidv4 } from 'uuid';
 
 const AddTask = () => {
+  const router = useRouter();
   const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [newTaskValue, setNewTaskValue] = useState<string>('');
 
-  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = (e) => {
+  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    console.log(newTaskValue);
+    await addTodo({
+      id: uuidv4(),
+      text: newTaskValue
+    })
+    setNewTaskValue("");
+    setModalOpen(false);
+    router.refresh();
   }
 
   return (
--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,16 +1,74 @@
+"use client";
+
+import { FaEdit, FaTrash } from "react-icons/fa";
 import { ITask } from '@/types/tasks'
-import React from 'react'
+import React, { FormEventHandler, useState } from 'react'
+import Modal from "./Modal";
+import { useRouter } from "next/navigation";
+import { deleteTodo, editTodo } from "@/api";
+
 
 interface TaskProps {
-    task:ITask
+  task: ITask
 }
 
 const Task: React.FC<TaskProps> = ({ task }) => {
+  const router = useRouter();
+  const [openModalEdit, setOpenModalEdit] = useState<boolean>(false);
+  const [openModalDeleted, setOpenModalDeleted] = useState<boolean>(false);
+  const [taskToEdit, setTaskToEdit] = useState<string>(task.text);
+
+  // Functions
+  const handleSubmitEditTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await editTodo({
+      id: task.id,
+      text: taskToEdit
+    })
+    setOpenModalEdit(false);
+    router.refresh();
+  }
+  const handleDeleteTask = async (id: string) => {
+    await deleteTodo(id);
+    setOpenModalDeleted(false);
+    router.refresh();
+  }
+
   return (
     <tr key={task.id}>
-    <td>{task.text}</td>
-    <td>Blue</td>
-  </tr>
+      <td className="w-full">{task.text}</td>
+      <td className="flex gap-6">
+        <FaEdit onClick={() => setOpenModalEdit(true)}
+          cursor={"pointer"}
+          size={25}
+          className="text-blue-500" />
+        <Modal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
+          <form onSubmit={handleSubmitEditTodo}>
+            <h3 className='font-bold text-lg'>Edit task</h3>
+            <div className='modal-action'>
+              <input type="text"
+                value={taskToEdit}
+                onChange={(e) => setTaskToEdit(e.target.value)}
+                placeholder="Type here"
+                className="input input-bordered w-full" />
+              <button type='submit' className='btn'>Submit</button>
+            </div>
+          </form>
+        </Modal>
+        <FaTrash onClick={() => setOpenModalDeleted(true)}
+          cursor={"pointer"} size={25}
+          className="text-red-500" />
+        <Modal modalOpen={openModalDeleted} setModalOpen={setOpenModalDeleted}>
+          <h3 className="text-lg">Are you sure, you want to delete this task?</h3>
+          <div className="modal-action">
+            <button
+              onClick={() => handleDeleteTask(task.id)}
+              className="btn">Yes
+            </button>
+          </div>
+        </Modal>
+      </td>
+    </tr>
   )
 }
 
